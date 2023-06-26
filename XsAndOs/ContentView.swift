50671544205a64f18572c39c3162623bd0ca21ca
Christos Eteoglou <itsmeglue@gmail.com>
2023-06-26 17:51:37 +0200

    Initial Commit

---
+++ XsAndOs/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  XsAndOs
+//
+//  Created by Christos Eteoglou on 2023-06-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XsAndOs/XsAndOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XsAndOsApp.swift
+//  XsAndOs
+//
+//  Created by Christos Eteoglou on 2023-06-26.
+//
+
+import SwiftUI
+
+@main
+struct XsAndOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
